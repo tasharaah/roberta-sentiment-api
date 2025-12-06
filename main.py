7079d5123eb410aa538dc8fbec6c83f1c1e7c7de
Naashrah Naashidin Preetasha <n.n.preetasha29@gmail.com>
2025-12-06 21:23:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_ID)
 
 model.config.id2label = {
-    0: "NEGATIVE",
-    1: "POSITIVE",
+    "LABEL_0" : "NEGATIVE",
+    "LABEL_1" : "POSITIVE",
 }
 model.config.label2id = {
-    "NEGATIVE": 0,
-    "POSITIVE": 1,
+    "NEGATIVE": "LABEL_0",
+    "POSITIVE": "LABEL_1",
 }
 
 model.eval()
